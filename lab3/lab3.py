b1663247ce903a0c5238aee89931249dc9d645c8
加贺加贺岬 <mengsiming77@gmail.com>
2022-03-25 10:25:45 +0800

    doc to be done

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -1,7 +1,5 @@
-from typing import Counter
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
 
 
 INF = 0
@@ -34,16 +32,17 @@ def create_data():
     return X, y
 
 
-def distance(x1, x2, p=INF, axis=0):
+def distance(x1, x2, p=2, axis=0):
     if p == INF:
-        return np.max(x1 - x2)
+        return np.max(x1 - x2, axis=axis)
     return np.sum(np.abs(x1 - x2) ** p, axis=axis) ** (1 / p)
 
 
 class KMeans:
-    def __init__(self, k=2, t=10) -> None:
+    def __init__(self, k=2, t=10, p=2) -> None:
         self.k = k
         self.t = t
+        self.p = p
 
     def fit(self, X, method=0):
         m = np.shape(X)[0]
@@ -64,10 +63,9 @@ class KMeans:
                 minJ = min(X[:, j])
                 rangeJ = float(max(X[:, j]) - minJ)
                 self.centroids[:, j] = np.array(minJ + rangeJ * np.random.rand(self.k))
-
         for t in range(self.t):
             for i, x in enumerate(X):
-                dis = distance(x, self.centroids, axis=1)
+                dis = distance(x, self.centroids, axis=1, p=self.p)
                 self.labs[i] = dis.argmin()
             stable = True
             for i in range(self.k):
@@ -99,18 +97,17 @@ class KMeans:
 
 
 group, labs = create_data()
-km = KMeans()
+km = KMeans(p=2, t=20)
 wss = []
-for k in range(2, 15):
+for k in range(1, 15):
     km.k = k
-    km.fit(group, method=0)
+    km.fit(group, method=2)
     w = km.wss()
-    print(km.wss())
     wss.append(w)
 print(wss)
 
 plt.figure(0)
-plt.plot(range(2, len(wss) + 2), wss)
+plt.plot(range(1, len(wss) + 1), wss)
 plt.xlabel("K")
 plt.ylabel("WSS")
 plt.show()
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from cmath import inf
 from typing import Counter
 import numpy as np
 import pandas as pd
@@ -15,16 +16,17 @@ def create_data():
     return X, y
 
 
-def distance(x1, x2, p=1, axis=0):
+def distance(x1, x2, p=2, axis=0):
     if p == INF:
-        return np.max(x1 - x2)
+        return np.max(x1 - x2, axis=axis)
     return np.sum(np.abs(x1 - x2) ** p, axis=axis) ** (1 / p)
 
 
 class KMeans:
-    def __init__(self, k=2, t=10) -> None:
+    def __init__(self, k=2, t=10, p=2) -> None:
         self.k = k
         self.t = t
+        self.p = p
 
     def fit(self, X, method=0):
         m = np.shape(X)[0]
@@ -45,10 +47,9 @@ class KMeans:
                 minJ = min(X[:, j])
                 rangeJ = float(max(X[:, j]) - minJ)
                 self.centroids[:, j] = np.array(minJ + rangeJ * np.random.rand(self.k))
-
         for t in range(self.t):
             for i, x in enumerate(X):
-                dis = distance(x, self.centroids, axis=)1
+                dis = distance(x, self.centroids, axis=1, p=self.p)
                 self.labs[i] = dis.argmin()
             stable = True
             for i in range(self.k):
@@ -80,19 +81,16 @@ class KMeans:
 
 
 group, labs = create_data()
-km = KMeans()
+km = KMeans(p=2, t=20, k=3)
 wss = []
-for k in range(2, 15):
-    km.k = k
-    km.t = 20
-    km.fit(group, method=0)
-    w = km.wss()
-    print(km.wss())
-    wss.append(w)
+km.fit(group, method=0)
+w = km.wss()
+wss.append(w)
 print(wss)
 
 plt.figure(0)
-plt.plot(range(2, len(wss) + 2), wss)
-plt.xlabel("K")
-plt.ylabel("WSS")
+plt.scatter(km.X[:, 2], km.X[:, 3], km.labs, cmap="brg")
+plt.xlabel("Petal.Length")
+plt.ylabel("Petal.Width")
 plt.show()
+print(km.labs)
